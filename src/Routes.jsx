e989bea7f946b928a91b8c2e3c19f0c40345e5aa
Yara Eldidi <yaraessameldindd@gmail.com>
2025-10-16 09:59:53 +0300

    feat: add Events page

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,9 +1,14 @@
+import EventsPage from "./pages/Events";
 import HomePage from "./pages/Home";
 
 const routes = [
     {
         path:"/",
         element: <HomePage/>
+    },
+    {
+        path:"/events",
+        element: <EventsPage/>
     }
 ];
 export { routes };
